40c046efba56879de7b8d4ff487279130aba498f
Yevhen <vffgdbvdgh@gmail.com>
2023-02-12 14:36:13 +0300

    add script for gmail

--- js/script.js
+++ js/script.js
@@ -28,13 +28,25 @@ $(document).ready(function(){
     });
     $('.button_price').on('click', function() {
         $('.modals, #button_price').fadeIn();
-    })
+    });
     $('.modals__close').on('click', function() {
         $('.modals, #button_price, #consultations').fadeOut();
-    })
+    });
     $('.button_price').each(function(i){
         $(this).on('click', function(){
             $('.modals h3').text($('h3').eq(i).text())
-        })
-    })
+        });
+    });
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
